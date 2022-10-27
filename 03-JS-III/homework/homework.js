5736c068406adc1be26abd8406eb56f9999d01f2
Manuel Lopez <manuel2016lopez.s@gmail.com>
2022-10-26 22:11:53 -0300

    Tercer completo

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,54 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acumulador = 0;
+    for(let i=0; i < 11; i++){
+      acumulador = acumulador +i;
+    }
+    return acumulador;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var resultado =[];
+  var indice= 0;
+  while(indice < array.length){
+     if(array[indice]%2===0){
+      resultado.push(array[indice])
+     }
+     indice++;
+  }
+  return resultado;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var alcuadrado=  array.map(function(elemento){
+    return elemento**2;
+  })
+  return alcuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma = array.reduce(function (acc, elemento) {
+    return acc + elemento;
+  })
+  return suma ;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numberString= num +"";
+  return numberString.length;
+
 }
   
   
